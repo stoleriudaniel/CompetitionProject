3811254a647b81463ac2ce68b0a2794cecd83ad0
stoleriudaniel <stoleriu.daniel47@gmail.com>
2022-01-24 10:11:23 -0800

    Add loop for communication

---
+++ Server/src/app/Server.java
@@ -0,0 +1,31 @@
+package app;
+
+import app.dao.PersonDao;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.*;
+
+public class Server {
+    public static final int PORT = 8100;
+    public Server() throws IOException {
+        ServerSocket serverSocket = null ;
+        try {
+            serverSocket = new ServerSocket(PORT);
+            while (true) {
+                System.out.println ("Waiting for a client ...");
+                Socket socket = serverSocket.accept();
+                // Execute the client's request in a new thread
+                new ClientThread(socket).start();
+            }
+        } catch (IOException e) {
+            System.err. println ("Ooops... " + e);
+        } finally {
+            serverSocket.close();
+        }
+    }
+    public static void main ( String [] args ) throws IOException {
+        Server server = new Server ();
+    }
+}

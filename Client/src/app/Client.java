71c281393de906a239bd486d2bce84dc9462b272
stoleriudaniel <stoleriu.daniel47@gmail.com>
2022-01-24 10:12:07 -0800

    Created Client

---
+++ Client/src/app/Client.java
@@ -0,0 +1,34 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+    public static void main (String[] args) throws IOException {
+        String serverAddress = "127.0.0.1"; // The server's IP address
+        int PORT = 8100; // The server's port
+        try (
+                Socket socket = new Socket(serverAddress, PORT);
+                PrintWriter out =
+                        new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader (
+                        new InputStreamReader(socket.getInputStream())) ) {
+            while(true) {
+                Scanner scanner = new Scanner(System.in);
+                String request = scanner.nextLine();
+                // Send a request to the server
+                out.println(request);
+                // Wait the response from the server ("Hello World!")
+                String response = in.readLine();
+                System.out.println(response);
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("No server listening... " + e);
+        }
+    }
+}

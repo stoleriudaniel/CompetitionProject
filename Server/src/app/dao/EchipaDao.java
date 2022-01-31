0b0456563c5484a68b73b71364346c12824220a6
stoleriudaniel <stoleriu.daniel47@gmail.com>
2022-01-31 14:05:18 +0200

    Added EchipaDao Methods

--- Server/src/app/dao/EchipaDao.java
+++ Server/src/app/dao/EchipaDao.java
@@ -1,4 +1,48 @@
 package app.dao;
 
+import app.model.Person;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EchipaDao {
+    public static void delete(int idEchipa, Connection conn){
+        try{
+            PreparedStatement stmt = conn.prepareStatement("DELETE FROM persons WHERE id_echipa="+idEchipa);
+            stmt.execute();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Stergere esuata!\n");
+        }
+    }
+    public static void updateIdEchipa(int idEchipaVechi, int idEchipaNou, Connection conn){
+        try{
+            PreparedStatement stmt = conn.prepareStatement("UPDATE persons SET id_echipa="+idEchipaNou+" WHERE id_echipa="+idEchipaVechi);
+            stmt.execute();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Stergere esuata!\n");
+        }
+    }
+    public static List<Person> read(int idEchipa, Connection conn){
+        List<Person> persons = new ArrayList<>();
+        try {
+            Statement statement = conn.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT id, username, id_echipa FROM persons WHERE id_Echipa="+idEchipa);
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String userName = resultSet.getString("username");
+                int idEchipaInt = resultSet.getInt("id_echipa");
+                Person person = new Person(id,userName,idEchipaInt);
+                persons.add(person);
+            }
+        } catch (Exception e){
+            System.out.println("Exceptie la read");
+            System.out.println(e.getCause());
+        }
+        return persons;
+    }
 }
